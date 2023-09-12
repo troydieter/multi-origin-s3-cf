38f9b151a32e13bce2203c2b11fc5c7a3712d06c
Troy Dieter <troydieter@gmail.com>
2023-09-12 16:50:25 -0400

    wip

--- multi_origin_s3_cf/multi_origin_s3_cf_stack.py
+++ multi_origin_s3_cf/multi_origin_s3_cf_stack.py
@@ -1,9 +1,10 @@
 from aws_cdk import (
     Stack, Tags,
 )
-from aws_cdk.aws_certificatemanager import Certificate
+from aws_cdk.aws_certificatemanager import Certificate, CertificateValidation
 from aws_cdk.aws_cloudfront import Distribution, BehaviorOptions
 from aws_cdk.aws_cloudfront_origins import S3Origin, OriginGroup
+from aws_cdk.aws_route53 import HostedZone, PublicHostedZone
 from aws_cdk.aws_s3 import Bucket, BucketEncryption
 from constructs import Construct
 
@@ -17,7 +18,7 @@ class MultiOriginS3CfStack(Stack):
         Tags.of(self).add("project", context["project"])
 
         top_level_domain = context["tld"]
-        primary_certificate = self.create_certificate(stage, cert_value=top_level_domain)
+        primary_certificate = self.create_certificate(stage, cert_value=f"*.{top_level_domain}", tld=top_level_domain)
 
         primary_origin_bucket = self.create_origin_bucket(stage, "primary_origin")
         secondary_origin_bucket = self.create_origin_bucket(stage, "secondary_origin")
@@ -27,7 +28,7 @@ class MultiOriginS3CfStack(Stack):
                          fallback_origin=S3Origin(secondary_origin_bucket)
                      )),
                      comment=f"{context['tld']}_distribution",
-                     domain_names=[f"{top_level_domain}", f"*.{top_level_domain}", f"www.${top_level_domain}"],
+                     domain_names=[f"*.{top_level_domain}", f"{top_level_domain}", f"www.${top_level_domain}"],
                      certificate=primary_certificate
                      )
 
@@ -39,8 +40,10 @@ class MultiOriginS3CfStack(Stack):
                       public_read_access=False,
                       versioned=True)
 
-    def create_certificate(self, stage: str, cert_value: str):
+    def create_certificate(self, stage: str, cert_value: str, tld: str):
+        imported_zone = PublicHostedZone.from_lookup(self, f"{cert_value}", domain_name=tld)
         return Certificate(self,
                            f"{cert_value}-{stage}",
                            domain_name=cert_value,
-                           subject_alternative_names=[f"*.{cert_value}", f"www.{cert_value}"])
+                           subject_alternative_names=[f"{tld}", f"www.{tld}"],
+                           validation=CertificateValidation.from_dns(hosted_zone=imported_zone))
