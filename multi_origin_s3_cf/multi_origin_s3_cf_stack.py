3d3af880523539f9d0630f7e6aebd11d44b89e9f
Troy Dieter <troydieter@gmail.com>
2023-09-12 16:00:50 -0400

    added cert

--- multi_origin_s3_cf/multi_origin_s3_cf_stack.py
+++ multi_origin_s3_cf/multi_origin_s3_cf_stack.py
@@ -1,6 +1,7 @@
 from aws_cdk import (
     Stack, Tags,
 )
+from aws_cdk.aws_certificatemanager import Certificate
 from aws_cdk.aws_cloudfront import Distribution, BehaviorOptions
 from aws_cdk.aws_cloudfront_origins import S3Origin, OriginGroup
 from aws_cdk.aws_s3 import Bucket, BucketEncryption
@@ -14,13 +15,20 @@ class MultiOriginS3CfStack(Stack):
         ###
         # Tag everything
         Tags.of(self).add("project", context["project"])
+
+        top_level_domain = context["tld"]
+        primary_certificate = self.create_certificate(stage, cert_value=top_level_domain)
+
         primary_origin_bucket = self.create_origin_bucket(stage, "primary_origin")
         secondary_origin_bucket = self.create_origin_bucket(stage, "secondary_origin")
         Distribution(self, f"{context['tld']}_distribution",
                      default_behavior=BehaviorOptions(origin=OriginGroup(
                          primary_origin=S3Origin(primary_origin_bucket),
-                         fallback_origin=S3Origin(secondary_origin_bucket),
-                     ))
+                         fallback_origin=S3Origin(secondary_origin_bucket)
+                     )),
+                     comment=f"{context['tld']}_distribution",
+                     domain_names=[f"{top_level_domain}", f"*.{top_level_domain}", f"www.${top_level_domain}"],
+                     certificate=primary_certificate
                      )
 
     def create_origin_bucket(self, stage: str, bucket_name: str):
@@ -30,3 +38,9 @@ class MultiOriginS3CfStack(Stack):
                       enforce_ssl=True,
                       public_read_access=False,
                       versioned=True)
+
+    def create_certificate(self, stage: str, cert_value: str):
+        return Certificate(self,
+                           f"{cert_value}-{stage}",
+                           domain_name=cert_value,
+                           subject_alternative_names=[f"*.{cert_value}", f"www.{cert_value}"])
